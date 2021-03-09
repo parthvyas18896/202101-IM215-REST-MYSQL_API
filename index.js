bb05a0d8adda7258ad61a3dce00624bc2e343cf5
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-03-09 00:23:17 -0500

    sybmitting files

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const express = require('express');
+
+const app = express()
+
+app.get('/', (request, response) => {
+    console.log('Request Received');
+    //response.send('welcome');
+    //response.json({msg: 'Welcome'});
+    response.end();
+
+});
+app.listen(3333,() => {
+    console.log('The server is up and listening on port 3333');
+});
