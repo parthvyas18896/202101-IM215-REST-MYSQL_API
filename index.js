e99637f17d0465f8314070241325ae9b9bcb2422
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-03-21 23:37:38 -0400

    Examples of multiple ways to receive parameter with diffrent Rest API method

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', (request, response) => {
 // //     response.end()
 // })
 app.get('/id/:user_id',(request,response) =>{
-     
+    console.log('###Get method');
     console.log('query parameters')
     console.log(request.query);
     
@@ -37,6 +37,78 @@ app.get('/id/:user_id',(request,response) =>{
     console.log('####Route parameters')
     console.log(request.params);
     response.end()
+})
+app.post('/id/:user_id',(request,response) =>{
+     console.log('###post method');
+    console.log('query parameters')
+    console.log(request.query);
+    
+    console.log("Body");
+    console.log(request.body);
+
+    console.log('####Route parameters')
+    console.log(request.params);
+    response.end()
+})
+app.put('/id/:user_id',(request,response) =>{
+    console.log('###put method');
+   console.log('query parameters')
+   console.log(request.query);
+   
+   console.log("Body");
+   console.log(request.body);
+
+   console.log('####Route parameters')
+   console.log(request.params);
+   response.end()
+})
+app.patch('/id/:user_id',(request,response) =>{
+    console.log('###patch method');
+   console.log('query parameters')
+   console.log(request.query);
+   
+   console.log("Body");
+   console.log(request.body);
+
+   console.log('####Route parameters')
+   console.log(request.params);
+   response.end()
+})
+app.delete('/id/:user_id',(request,response) =>{
+    console.log('###delete method');
+   console.log('query parameters')
+   console.log(request.query);
+   
+   console.log("Body");
+   console.log(request.body);
+
+   console.log('####Route parameters')
+   console.log(request.params);
+   response.end()
+})
+app.options('/id/:user_id',(request,response) =>{
+    console.log('###options method');
+   console.log('query parameters')
+   console.log(request.query);
+   
+   console.log("Body");
+   console.log(request.body);
+
+   console.log('####Route parameters')
+   console.log(request.params);
+   response.end()
+})
+app.head('/id/:user_id',(request,response) =>{
+    console.log('###head method');
+   console.log('query parameters')
+   console.log(request.query);
+   
+   console.log("Body");
+   console.log(request.body);
+
+   console.log('####Route parameters')
+   console.log(request.params);
+   response.end()
 })
 app.listen(3333,() => {
     console.log('The server is up and listening on port 3333');
